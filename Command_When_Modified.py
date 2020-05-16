1502d162ad623ed23314c496c57afd1d2254bac1
omarjso <omarshaikhomar@gmail.com>
2020-05-15 22:40:31 -0700

    Add command line arguments

--- Command_When_Modified.py
+++ Command_When_Modified.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python3
 import os
 import time
+import sys
 
 
-def main():
-	filename = input("Name of the watched file: ")
-	command = input("Command to excute on file: ")
+def main(filename):
+    if filename == "":
+        filename = input("Name of the watched file: ")
+    command = input("Command to excute on file: ")
 
-	lastMod = os.stat(filename).st_mtime
-	while True:
-		time.sleep(3)
-		
-		# Sometimes after saving a file the system needs time to recognize the
-		# new file leading it to believe that it doesn't exist. This try, 
-		# except catches that.
-		try:
-			newTime = os.stat(filename).st_mtime
-			if newTime > lastMod:
-				os.system(command)
-				lastMod = newTime
+    lastMod = os.stat(filename).st_mtime
+    while True:
+        time.sleep(1)
 
-		except FileNotFoundError:
-			continue
+        # Sometimes after saving a file the system needs time to
+        # recognize the new file leading it to believe that it doesn't
+        # exist. This try, except catches that.
+        try: 
+            newTime = os.stat(filename).st_mtime 
+            if newTime > lastMod:
+                os.system(command)
+                lastMod = newTime
+        except FileNotFoundError:
+            continue
 
 if __name__ == "__main__":
-	main()
+    main(sys.argv[1])
