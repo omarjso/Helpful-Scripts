95d773d146ba2c76053fdbb37f798a22c69d02eb
omarjso <omarshaikhomar@gmail.com>
2020-05-20 02:08:12 -0700

    Add bash alias whenever new class is created

--- Create_Classes.py
+++ Create_Classes.py
@@ -4,7 +4,14 @@ import sys
 import re
 
 CLASSES_DIR = "{}/Documents/UWcourses".format(os.path.expanduser("~"))
+CLASS_ALIASES = "{}/.aliases/.classes".format(os.path.expanduser("~"))
 
+def addBashAlias(class_code, class_path):
+    code = class_code.split("-")[0]
+    path = class_path.replace(os.path.expanduser("~"), "~")
+    alias_command = 'alias {}="cd {}"\n'.format(code, path)
+    with open(CLASS_ALIASES, "a") as f:
+            f.write(alias_command)
 
 def className():
     clsName = input("What class do you want to add? ")
@@ -36,13 +43,16 @@ def createClassDir(code):
     os.makedirs("{}/Lectures".format(class_path))
     os.makedirs("{}/Sections".format(class_path))
     os.makedirs("{}/HW".format(class_path))
+    return class_path
 
 def main(argv):
     if not argv:
         argv = [className()]
 
     for cls in argv:
-        createClassDir(classCode(cls))
+        class_code = classCode(cls)
+        class_path = createClassDir(class_code)
+        addBashAlias(class_code, class_path)
 
     print("classes are added!")
 
